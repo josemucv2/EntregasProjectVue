e22a97c61d40f2ec5181ab9e79aa8df3cf7ea297
josemucv2 <martinezcaceresjosem@gmail.com>
2022-05-10 00:38:45 -0400

    entrega2

--- App.js
+++ App.js
@@ -1,33 +1,40 @@
 
 Vue.component('main-users', {
 
-    props :[
-        'listUser',
-        'addClass',
-        
-    ],
+    props :{ 
+        titulos: {
+            type: Array,
+            required: true
+        },
+        registros: {
+            type: Array,
+            required: true
+        }
+    },
 
     template: `
-    <table class="table style-table">
-    <thead>
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Nombre</th>
-            <th scope="col">Apellido</th>
-            <th scope="col">Pais</th>
-        </tr>
-    </thead>
-
-  <tbody>
-    <tr v-for="( item, index ) in listUser" :key="index" :class="addClass">
-      <th scope="row">{{item.id}}</th>
-      <td>{{item.nombre}}</td>
-      <td>{{item.Apellido}}</td>
-      <td>{{item.Pais}}</td>
-    </tr>
-    </tbody>
-</table>
+    <div>
+        <table class="table table-striped table-dark">
+            <thead class="thead-light">
+                <tr>
+                    <th v-for="(item, i) in titulos" :key="i" scope="col">{{ item }}</th>
+                </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(reg, indice) in registros" :key="indice">
+                
+               <th v-for="(dato,d) in reg" :key="d">{{dato}}</th>
+            
+            
+            </tr>
+        
+        </tbody>
+        </table>
+        
     
+    
+    </div>
+
     `
 })
 
@@ -37,13 +44,28 @@ var app = new Vue({
 
     data:{
         menu: false,
-        listUser: [
-            {id: 1, nombre: 'Jose', Apellido: 'Martinez', Pais: 'Venezuela'},
-            {id: 2, nombre: 'Juan', Apellido: 'Perez', Pais: 'Colombia'},
-            {id: 3, nombre: 'Alexander', Apellido: 'Rincon', Pais: 'Argentina'},
-            {id: 4, nombre: 'Rocky', Apellido: 'Moncada', Pais: 'Chile'},
-            {id: 5, nombre: 'Milton', Apellido: 'Avel', Pais: 'Peru'},
-        ],
+        // listUser: [
+
+
+        //     {id: 1, nombre: 'Jose', Apellido: 'Martinez', Pais: 'Venezuela'},
+        //     {id: 2, nombre: 'Juan', Apellido: 'Perez', Pais: 'Colombia'},
+        //     {id: 3, nombre: 'Alexander', Apellido: 'Rincon', Pais: 'Argentina'},
+        //     {id: 4, nombre: 'Rocky', Apellido: 'Moncada', Pais: 'Chile'},
+        //     {id: 5, nombre: 'Milton', Apellido: 'Avel', Pais: 'Peru'},
+            
+        // ],
+
+        tabla1: {
+            titulos: ["ID", "Nombre", "Apellido", "Pais"],
+             registros: [
+                [1, "Jose", "Martinez", "Venezuela"],
+                [2, "Juan", "Perez", "Colombia"],
+                [3, "Alexander", "Rincon", "Argentina"],
+                [4, "Rocky", "Moncada", "Chile"],
+                [5, "Milton", "Avel", "Peru"],
+            ],
+        } , 
+        
 
          
  
@@ -63,8 +85,13 @@ var app = new Vue({
             return false
         },
 
-        
-
+        fondos(i){
+            if(i%2 == 0){
+                return 'numberPar'
+            } else {
+                return 'numberImpar'
+            }
+        }
         
 
        
